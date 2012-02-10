23fcc14350a709ecf1eea250e75a0af99074eecf
Gabor Szabo <szabgab@gmail.com>
2012-02-10 13:13:44 +0200

    Wx::Scintilla      0.36

---
+++ perl/site/lib/Wx/Scintilla/TextCtrl.pm
@@ -0,0 +1,10 @@
+package Wx::Scintilla::TextCtrl;
+
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.36';
+our @ISA     = 'Wx::ScintillaTextCtrl';
+
+1;
---
+++ perl/site/lib/Wx/Scintilla/TextEvent.pm
@@ -0,0 +1,10 @@
+package Wx::Scintilla::TextEvent;
+
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.36';
+our @ISA     = 'Wx::ScintillaTextEvent';
+
+1;
