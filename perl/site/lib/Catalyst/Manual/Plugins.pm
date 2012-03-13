9dba62888d71fc473280161a39d9413d350eb81f
Gabor Szabo <szabgab@gmail.com>
2012-03-13 15:08:50 +0200

    Catalyst::Manual      5.9003

---
+++ perl/site/lib/Catalyst/Manual/Plugins.pm
@@ -0,0 +1,19 @@
+=head1 NAME
+
+Catalyst::Manual::Plugins - DEPRECATED, see L<Catalyst::Manual::Components> instead .
+
+=head1 SEE INSTEAD
+
+L<Catalyst::Manual::Components>
+
+=head1 AUTHORS
+
+Catalyst Contributors, see Catalyst.pm
+
+=head1 COPYRIGHT
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
